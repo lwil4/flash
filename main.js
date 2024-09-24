1107df4482ac15293678663aeb8072cf6078c54e
lwil4 <159133011+lwil4@users.noreply.github.com>
2024-09-24 08:22:38 -0300

    d

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
+    <div class="cartao-conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao-pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao-resposta"></div>
+        <p>${resposta}</p>
+    </div>
+    </div>
     `
-    
+container.appendChild
 }
