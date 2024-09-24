2efe9a7dec9705ce01b22ea524ea92c7d08ca533
lwil4 <159133011+lwil4@users.noreply.github.com>
2024-09-24 07:29:57 -0300

    d

---
+++ perguntas.js
@@ -0,0 +1,80 @@
+criaCartao(
+    'Física,
+    'Como se calcula a velocidade média?',
+    'Pode-se utilizar a fórmula V=🔺️S/🔺️t para o cálculo. ')
+    criaCartao(
+    'Física,
+    'Como se calcula a Frequência de uma onda?',
+    'Pode-se utilizar a fórmula f= 1/T para o cálculo')
+    criaCartao(
+    'Física,
+    'Como se calcula a velocidade de uma onda?',
+    'Pode-se utilizar a fórmula v= λ.f para o cálculo')
+    criaCartao(
+    'Física,
+    'Como se calcula a quantidade de movimento?',
+    'Pode-se utilizar a fórmula Q=m.v para o cálculo')
+    criaCartao(
+    'Física,
+    'Como se calcula um Impulso?',
+    'Pode-se utilizar a fórmula I= F.🔺️t para o cálculo')
+    criaCartao(
+    'Matemática',
+    'Como se calcula a hipotenusa?',
+    'Pode-se utilizar a fórmula de Pitágoras a²= b² + c² para o cálculo')
+    criaCartao(
+    'Matemática',
+    'Como se calcula o Delta (🔺️)?',
+    'Pode-se utilizar a fórmula 🔺️= b² - 4.a.c para o cálculo')
+    criaCartao(
+    'Matemática',
+    'Como se calcula a área de um retângulo?',
+    'Pode-se utilizar a fórmula A= b.h para o cálculo')
+    criaCartao(
+    'Matemática',
+    'Como se calcula a Área de um triângulo?',
+    'Pode-se utilizar a fórmula A= b.h/2 para o cálculo')
+    criaCartao(
+    'Matemática',
+    'Como se calcula o volume de uma esfera?',
+    'Pode-se utilizar a fórmula V= 4/3.π.r³ para o cálculo')
+    criaCartao(
+    'Biologia',
+    'Como é a transmissão da Leishmaniose?',
+    'É por meio da picada de um mosquito fêmea infectado com a doença')
+    criaCartao(
+    'Biologia',
+    'Como é a transmissão da dengue?',
+    'É por meio da transmissão de um mosquito infectado com a doença, geralmente Aedes Egypt')
+    criaCartao(
+    'Biologia',
+    'Como é a transmissão da lombriga?',
+    'Por meio da ingestão de água ou alimentos infectados com ovos do lumbricoides')
+    criaCartao(
+    'Biologia',
+    'Como é a transmissão da AIDS',
+    'Pode ser por meio de transfusão de sangue, relações sexuais e compartilhamento de objetos perfurantes contaminados.')
+    criaCartao(
+    'Biologia',
+    'Como é a transmissão da ancilostomose?',
+    'Por meio do contato com solos contaminados com as larvas da doença, que penetram na pele e transitam pelo corpo.')
+    criaCartao(
+    'Química',
+    'Quem denominou o átomo como pudim de passas?',
+    'Thomson denominou.')
+    criaCartao(
+    'Química',
+    'Quem denominou o átomo como sendo maciço?',
+    'Dalton denominou.')
+    criaCartao(
+    'Química',
+    'Quem disse que o átomo possuia uma eletrosfera?',
+    'Rutherford disse.')
+    criaCartao(
+    'Química',
+    'Quem atribuiu pela primeira vez a palavra ÁTOMO?',
+    'Leucipo e Demócrito.')
+    criaCartao(
+    'Química',
+    'O que faz uma pilha funcionar?',
+    'O cátodo e o ânodo são elementos da pilha que juntos atribuem funcionamento à pilha devido aos sinais opostos')
