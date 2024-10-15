714964601666483e1b69170db575f411125d4163
lwil4 <159133011+lwil4@users.noreply.github.com>
2024-10-15 08:29:01 -0300

    s

--- projeto2.js
+++ projeto2.js
@@ -17,7 +17,7 @@ function criaCartao (categoria, pergunta, resposta){
 }
 
 function showAnswer(card){
-    const answer = document.querySelector('.cartao-resposta');
+    const answer = card.querySelector('.cartao-resposta');
     if (answer.style.display === 'none'){
         answer.style.display = 'block';
     }
